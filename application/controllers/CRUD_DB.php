bc68d58e9a825fce952e7b853b5a16a8587b53d1
daniel0327 <d-cia@hotmail.com>
2016-04-12 03:01:10 -0500

    modificación pruebas unitarias
    se crean pruebas unitarias adicionales a las que ya existian

--- application/controllers/CRUD_DB.php
+++ application/controllers/CRUD_DB.php
@@ -39,6 +39,130 @@
 
 		}
 
+public function eliminarDerechodePeticion()
+		{
+
+			
+			
+			//PDO Database Connection
+			try {
+				$databaseConnection = new PDO('mysql:host='._HOST_NAME_.';dbname='._DATABASE_NAME_, _USER_NAME_, _DB_PASSWORD);
+				$databaseConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			} catch(PDOException $e) {
+				echo 'ERROR: ' . $e->getMessage();
+			}
+
+				$records = $databaseConnection->prepare("DELETE FROM `derechopeticion` WHERE (`Radicado_Interno` = 5435 AND               `Usuario_codigo_rol` = 1)");
+				$records->execute();
+
+				
+					$databaseConnection = null;
+
+					return "derecho de peticion eliminado";
+
+		}
+
+
+public function crearTutela()
+{
+	
+	
+	
+	//PDO Database Connection
+	try {
+		$databaseConnection = new PDO('mysql:host='._HOST_NAME_.';dbname='._DATABASE_NAME_, _USER_NAME_, _DB_PASSWORD);
+		$databaseConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	} catch(PDOException $e) {
+		echo 'ERROR: ' . $e->getMessage();
+	}
+
+	$records = $databaseConnection->prepare("DELETE FROM `tutela` WHERE ((`Radicado_Interno` = 34234 AND `Usuario_codigo_rol` = 1) OR (`Radicado_Interno` = 988773 AND `Usuario_codigo_rol` = 1))");
+		$records->execute();
+
+
+	$records = $databaseConnection->prepare("INSERT INTO `tutela` (`Radicado_Interno`, `Radicado_UQ`, `Asunto`, `Solicitante`, 										`Responsable`, `Correo_Responsable`, `Fecha_Recibido`, `Fecha_Vencimiento`, `Usuario_codigo_rol`) VALUES	(34234, 5435, 'devolucion de dinero', 'jhon', '												jorge', 'jose@hotmail.com', '2016-01-20', '2016-03-22', 1), (988773, 65534, '											integracion a la universidad', 'manuel', 'luis', 'uq@hotmail.com', '2016-02-20', '										2016-03-9', 1)");
+			
+			$records->execute();
+
+			$databaseConnection = null;
+
+			return "tutelas creadas";
+
+}
+
+public function eliminarTutela()
+{
+	
+	
+	
+	//PDO Database Connection
+	try {
+		$databaseConnection = new PDO('mysql:host='._HOST_NAME_.';dbname='._DATABASE_NAME_, _USER_NAME_, _DB_PASSWORD);
+		$databaseConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	} catch(PDOException $e) {
+		echo 'ERROR: ' . $e->getMessage();
+	}
+
+	$records = $databaseConnection->prepare("DELETE FROM `tutela` WHERE (`Radicado_Interno` = 34234 AND `Usuario_codigo_rol` = 1)");
+		$records->execute();
+
+
+			$databaseConnection = null;
+
+			return "tutela eliminada";
+
+}
+
+public function crearUsuario()
+{	
+
+	
+	//PDO Database Connection
+	try {
+		$databaseConnection = new PDO('mysql:host='._HOST_NAME_.';dbname='._DATABASE_NAME_, _USER_NAME_, _DB_PASSWORD);
+		$databaseConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	} catch(PDOException $e) {
+		echo 'ERROR: ' . $e->getMessage();
+	}
+	
+	
+	$records = $databaseConnection->prepare("DELETE FROM `usuario` WHERE `codigo_rol` = 2;");
+		$records->execute();
+
+	$records = $databaseConnection->prepare("INSERT INTO `usuario` (`codigo_rol`, `perfil`, `username`, `passsword`) VALUES
+											(2, 'administrador', 'admin', 'admin');");
+			
+			$records->execute();
+
+
+			$databaseConnection = null;
+
+			return "usuario creado";
+
+}
+
+public function eliminarUsuario()
+{	
+
+	
+	//PDO Database Connection
+	try {
+		$databaseConnection = new PDO('mysql:host='._HOST_NAME_.';dbname='._DATABASE_NAME_, _USER_NAME_, _DB_PASSWORD);
+		$databaseConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	} catch(PDOException $e) {
+		echo 'ERROR: ' . $e->getMessage();
+	}
+	
+	
+	$records = $databaseConnection->prepare("DELETE FROM `usuario` WHERE `codigo_rol` = 2;");
+		$records->execute();
+
+			$databaseConnection = null;
+
+			return "usuario eliminado";
+
+}
+
 	}
 
 ?>
--- application/controllers/test.php
+++ application/controllers/test.php
@@ -9,6 +9,11 @@ class CRUDTest extends PHPUnit_Framework_TestCase
     // prueba para asegurarse de que el objeto de un fsockopen es válido 
     $connObj = new CRUD_DB();
     $this->assertTrue($connObj->crearDerechodePeticion() == "derechos de peticion creados");
+    $this->assertTrue($connObj->crearTutela() == "tutelas creadas");
+    $this->assertTrue($connObj->crearUsuario() == "usuario creado");
+    $this->assertTrue($connObj->eliminarDerechodePeticion() == "derecho de peticion eliminado");
+    $this->assertTrue($connObj->eliminarTutela() == "tutela eliminada");
+    $this->assertTrue($connObj->eliminarUsuario() == "usuario eliminado");
   }
 }
 ?>
